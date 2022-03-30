ebb2710383a9cfba1241b773746ced70c577312e
Scott Grosch <Scott.Grosch@icloud.com>
2022-03-30 08:57:27 -0700

    x

--- 08-handling-common-scenarios/projects/final/PushNotifications/PushNotifications/PushNotificationsApp.swift
+++ 08-handling-common-scenarios/projects/final/PushNotifications/PushNotifications/PushNotificationsApp.swift
@@ -5,10 +5,13 @@ struct PushNotificationsApp: App {
   @UIApplicationDelegateAdaptor
   private var appDelegate: AppDelegate
 
+  let controller = PersistenceController.shared
+
   var body: some Scene {
     WindowGroup {
       ContentView()
         .environmentObject(appDelegate.notificationCenter)
+        .environment(\.managedObjectContext, controller.container.viewContext)
     }
   }
 }
